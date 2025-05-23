abd33fe69a02b2435c567846ec025b9688bb8703
a23299 <aluno23299@ipt.pt>
2025-05-23 16:05:39 +0100

    Added UsersApiController.cs

--- BlocoNotas/BlocoNotas/Controllers/Api/UsersApiController.cs
+++ BlocoNotas/BlocoNotas/Controllers/Api/UsersApiController.cs
@@ -1,12 +1,103 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlocoNotas.Data;
+using BlocoNotas.Models;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
-namespace BlocoNotas.Controllers.Api;
-
-public class UsersApiController : Controller
+namespace BlocoNotas.Controllers.Api
 {
-    // GET
-    public IActionResult Index()
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersApiController : ControllerBase
     {
-        return View();
+        private readonly ApplicationDbContext _context;
+
+        public UsersApiController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UsersApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/UsersApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        // POST: api/UsersApi
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.UserId }, user);
+        }
+
+        // PUT: api/UsersApi/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(int id, User user)
+        {
+            if (id != user.UserId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/UsersApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.UserId == id);
+        }
     }
 }
