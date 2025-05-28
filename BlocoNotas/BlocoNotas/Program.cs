383fe32c0721a5f1f0bdfdc1f84bd96bb037dc21
Jorge Lavado <aluno23037@ipt.pt>
2025-05-28 11:31:25 +0100

    DB 1.1

--- BlocoNotas/BlocoNotas/Program.cs
+++ BlocoNotas/BlocoNotas/Program.cs
@@ -13,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(
-    options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
