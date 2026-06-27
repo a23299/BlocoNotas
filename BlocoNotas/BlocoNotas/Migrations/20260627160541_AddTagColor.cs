144bf6eabfb311862de652606e9a53c7a1994bd1
Jorge Lavado <aluno23037@ipt.pt>
2026-06-27 17:54:28 +0100

    Adiconar cores nas Tags e aba API

---
+++ BlocoNotas/BlocoNotas/Migrations/20260627160541_AddTagColor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlocoNotas.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTagColor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Tags",
+                type: "TEXT",
+                maxLength: 7,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Tags");
+        }
+    }
+}
